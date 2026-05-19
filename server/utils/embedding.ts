e834fac1882eca8af6b77cdc0ef763b97b678ba9
Marco <marco.nc.arruda@gmail.com>
2026-05-19 16:35:33 -0400

    fix: embeding util fn

--- server/utils/embedding.ts
+++ server/utils/embedding.ts
@@ -1,21 +1,22 @@
-import { sql } from "kysely";
-
-const config = useRuntimeConfig();
+import { sql, type RawBuilder } from "kysely";
+import ai from "./ai";
 
 export const embedText = async (summary: string): Promise<number[]> => {
+    const config = useRuntimeConfig();
+    const outputDimensionality = config.geminiEmbeddingDimensionality;
+
     const response = await ai.models.embedContent({
         model: config.geminiEmbeddingModel,
         contents: summary,
-        config: {
-            outputDimensionality: config.geminiEmbeddingDimensionality,
-        }
+        config: { outputDimensionality }
     })
 
     if (!response.embeddings) throw new Error("Embedding generation failed: No embeddings returned");
     if (response.embeddings.length === 0) throw new Error("Embedding generation failed: Empty embeddings array");
     if (!response.embeddings[0]!.values) throw new Error("Embedding generation failed: No values in the first embedding");
 
-    const embeddingValues = response.embeddings[0]!.values;
+    const values = response.embeddings[0]!.values;
+    const literal = `'${JSON.stringify(values)}'::vector(${outputDimensionality})`
 
-    return sql`${JSON.stringify(embeddingValues)}::vector(${config.geminiEmbeddingDimensionality})` as unknown as number[];
+    return sql.raw(literal) as unknown as number[]
 }
